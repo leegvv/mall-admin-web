d99875cbbd13181778b072904aa81c7eb56ef980
李谷 <ligu@rlair.net>
2020-04-28 13:18:50 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
 import Vue from 'vue'
-import App from './App.vue'
+import 'normalize.css/normalize.css'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css'
+import locale from 'element-ui/lib/locale/lang/zh-CN';
+import VCharts from 'v-charts';
+import "@/styles/index.scss";
+
+import App from './App.vue';
+import router from './router';
+import store from './store';
+
+Vue.use(ElementUI, {locale});
+Vue.use(VCharts);
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
+  store,
   render: h => h(App),
 }).$mount('#app')
